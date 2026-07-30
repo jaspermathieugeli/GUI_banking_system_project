279e352ea6b918f24ec7500639413c20636752dd
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-07-30 15:16:32 +0800

    add image variables

--- GUI_banking_system.py
+++ GUI_banking_system.py
@@ -4,3 +4,7 @@ from PIL import Image, ImageTk
 
 master = Tk()
 master.title("Banking System")
+
+image = Image.open("Images/bank.png")
+image = image.resize((200, 200))
+image = ImageTk.PhotoImage(image)
