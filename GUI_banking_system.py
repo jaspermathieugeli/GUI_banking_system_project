1cc5f2da036d2bbf68fb6f28b5b4d36eb86f68f1
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-07-30 16:01:14 +0800

    add variables and entries in create function

--- GUI_banking_system.py
+++ GUI_banking_system.py
@@ -6,12 +6,18 @@ master = Tk()
 master.title("Banking System")
 
 def create():
+
+    name = StringVar()
+    passcode = StringVar()
     
     creation_screen = Toplevel(master)
     creation_screen.title("Create")
 
     Label(creation_screen, text="Name", font=("Calibri", 10)).grid(row=1, sticky=W)
     Label(creation_screen, text="Passcode", font=("Calibri", 10)).grid(row=2, sticky=W)
+
+    Entry(creation_screen, textvariable=name).grid(row=1, column=1)
+    Entry(creation_screen, textvariable=passcode, show="*").grid(row=2, column=1)
 
 def log_in():
     pass
