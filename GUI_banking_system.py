64a59038bbc7821e9f929c5132f15cedb6ff6f5a
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-07-30 15:49:56 +0800

    call mainloop method

--- GUI_banking_system.py
+++ GUI_banking_system.py
@@ -14,3 +14,5 @@ Label(master, image=image).grid(row=2, sticky=N, pady=20)
 
 Button(master, text="Create", font=("Calibri", 10), width=20).grid(row=3, sticky=N, pady=5)
 Button(master, text="Log In", font=("Calibri", 10), width=20).grid(row=4, sticky=N, pady=5)
+
+master.mainloop()
