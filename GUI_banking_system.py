ad9463a9a1c148c0ab1c46dba970033e9581d4e7
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-07-30 15:36:21 +0800

    add buttons

--- GUI_banking_system.py
+++ GUI_banking_system.py
@@ -11,3 +11,6 @@ image = ImageTk.PhotoImage(image)
 
 Label(master, text = "Banking System", font=("Calibri", 10)).grid(row=0, sticky=N, pady=10)
 Label(master, image=image).grid(row=2, sticky=N, pady=20)
+
+Button(master, text="Create", font=("Calibri", 10), width=20).grid(row=3, sticky=N, pady=5)
+Button(master, text="Log In", font=("Calibri", 10), width=20).grid(row=4, sticky=N, pady=5)
