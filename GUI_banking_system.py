a93097607648781ee5c0493f8218660afee52f68
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-07-30 15:50:24 +0800

    add functions

--- GUI_banking_system.py
+++ GUI_banking_system.py
@@ -5,6 +5,11 @@ from PIL import Image, ImageTk
 master = Tk()
 master.title("Banking System")
 
+def create():
+    pass
+def log_in():
+    pass
+
 image = Image.open("Images/bank.png")
 image = image.resize((200, 200))
 image = ImageTk.PhotoImage(image)
@@ -12,7 +17,7 @@ image = ImageTk.PhotoImage(image)
 Label(master, text = "Banking System", font=("Calibri", 10)).grid(row=0, sticky=N, pady=10)
 Label(master, image=image).grid(row=2, sticky=N, pady=20)
 
-Button(master, text="Create", font=("Calibri", 10), width=20).grid(row=3, sticky=N, pady=5)
-Button(master, text="Log In", font=("Calibri", 10), width=20).grid(row=4, sticky=N, pady=5)
+Button(master, text="Create", font=("Calibri", 10), width=20, command=create).grid(row=3, sticky=N, pady=5)
+Button(master, text="Log In", font=("Calibri", 10), width=20, command=log_in).grid(row=4, sticky=N, pady=5)
 
 master.mainloop()
