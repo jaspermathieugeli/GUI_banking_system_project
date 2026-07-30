5f2e42041ea147059541c6aa853d1307299ed2f0
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-07-30 16:03:37 +0800

    add function in create function

--- GUI_banking_system.py
+++ GUI_banking_system.py
@@ -19,6 +19,8 @@ def create():
     Entry(creation_screen, textvariable=name).grid(row=1, column=1)
     Entry(creation_screen, textvariable=passcode, show="*").grid(row=2, column=1)
 
+    Button(creation_screen, text="Create", font=("Calibri", 10)).grid(row=3, sticky=N, pady=10)
+
 def log_in():
     pass
 
