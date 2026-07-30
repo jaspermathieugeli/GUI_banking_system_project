15fef807d87d4cf94ce72db48c9c9535d2ec0e53
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-07-30 15:24:39 +0800

    add labels

--- GUI_banking_system.py
+++ GUI_banking_system.py
@@ -8,3 +8,6 @@ master.title("Banking System")
 image = Image.open("Images/bank.png")
 image = image.resize((200, 200))
 image = ImageTk.PhotoImage(image)
+
+Label(master, text = "Banking System", font=("Calibri", 10)).grid(row=0, sticky=N, pady=10)
+Label(master, image=image).grid(row=2, sticky=N, pady=20)
